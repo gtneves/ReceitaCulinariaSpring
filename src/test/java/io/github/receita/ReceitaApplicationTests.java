367f3afedacc88ea8493dfc1399066da29b24af0
Caio Neves Monteiro <cenioo123@gmail.com>
2026-06-02 23:28:33 -0300

    Initial commit

---
+++ src/test/java/io/github/receita/ReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.receita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
